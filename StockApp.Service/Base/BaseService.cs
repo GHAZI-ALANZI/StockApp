78c184a855ac3b81db7289efdad2ecab72d06402
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-12-10 20:33:03 +0100

    Service and Mapper Class lib re done

---
+++ StockApp.Service/Base/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using StockApp.Core.UnitOfWorks;
+
+namespace StockApp.Service.Base
+{
+    public class BaseService
+    {
+        public readonly IUnitOfWorks _unitOfWork;
+        public readonly IMapper _mapper;
+
+        public BaseService(IUnitOfWorks unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+    }
+}
